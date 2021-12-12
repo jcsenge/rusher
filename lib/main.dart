dec38d1cd80ab8199a3b881c7848422f05f269fc
Csenge <jakabos11@gmail.com>
2021-12-12 15:38:42 +0100

    feat(*): main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
-import 'package:flutter/foundation.dart';
-import 'package:hive/hive.dart';
-import 'package:flame/game.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:rusher/components/login/login.dart';
+import 'package:rusher/components/login/login_page.dart';
+import 'package:rusher/components/login/register.dart';
 
-import 'components/menus/game_over_menu.dart';
 import 'components/gameplay/infobar.dart';
+import 'components/gameplay/rusher.dart';
+import 'components/menus/game_over_menu.dart';
 import 'components/menus/main_menu.dart';
 import 'components/menus/pause_menu.dart';
-import 'components/gameplay/rusher.dart';
 import 'components/menus/settings_menu.dart';
 import 'models/player_data.dart';
-import 'models/settings.dart';
 
 Rusher _rusher = Rusher();
 
@@ -22,6 +24,12 @@ Future<void> main() async {
   Flame.device.fullScreen();
   Flame.device.setLandscape();
   await initHive();
+  //await dotenv.load();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      
+    ),
+  );
   runApp(const RusherApp());
 }
 
@@ -32,13 +40,11 @@ Future<void> initHive() async {
   }
 
   Hive.registerAdapter<PlayerData>(PlayerDataAdapter());
-  Hive.registerAdapter<Settings>(SettingsAdapter());
 }
 
 class RusherApp extends StatelessWidget {
   const RusherApp({Key? key}) : super(key: key);
   final RusherGame rusherGame = const RusherGame();
-  final Login login = const Login();
 
   @override
   Widget build(BuildContext context) {
@@ -46,24 +52,26 @@ class RusherApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Rusher',
       theme: ThemeData(
+        errorColor: Colors.redAccent,
+        inputDecorationTheme:
+            const InputDecorationTheme(filled: true, fillColor: Colors.white54),
         fontFamily: 'Audiowide',
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.brown,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
-            fixedSize: const Size(200, 60),
-          ),
+              padding: const EdgeInsets.all(10.0),
+              fixedSize: const Size(200, 40),
+              primary: Colors.white54),
         ),
       ),
-      home: login,
+      home: rusherGame,
     );
   }
 }
 
 class RusherGame extends StatelessWidget {
   const RusherGame({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -75,13 +83,15 @@ class RusherGame extends StatelessWidget {
           ),
         ),
         overlayBuilderMap: {
+          LoginPage.id: (_, Rusher gameRef) => LoginPage(gameRef),
           MainMenu.id: (_, Rusher gameRef) => MainMenu(gameRef),
           PauseMenu.id: (_, Rusher gameRef) => PauseMenu(gameRef),
           InfoBar.id: (_, Rusher gameRef) => InfoBar(gameRef),
           GameOverMenu.id: (_, Rusher gameRef) => GameOverMenu(gameRef),
           SettingsMenu.id: (_, Rusher gameRef) => SettingsMenu(gameRef),
+          Register.id: (_, Rusher gameRef) => Register(gameRef),
         },
-        initialActiveOverlays: const [MainMenu.id],
+        initialActiveOverlays: const [LoginPage.id],
         game: _rusher,
       ),
     );
